c3e008f43752980652f03fe59eee2bbc2a5b165f
Vivek Rawat <vivek.rawat888@gmail.com>
2023-11-13 13:01:05 +0530

    swtiched to zig build system

--- build.zig
+++ build.zig
@@ -1,70 +1,50 @@
 const std = @import("std");
+const fs = std.fs;
+const print = std.debug.print;
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
+const day_start = 1;
+const day_end = 24;
+
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "aoc-zig",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
+    for (day_start..day_end) |day_number| {
+        const day_string = b.fmt("day{:0>2}", .{day_number});
+        const source_file_path = b.fmt("src/2022/{s}/main.zig", .{day_string});
+        const exe_name = b.fmt("aoc-{s}", .{day_string});
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
-    b.installArtifact(exe);
+        // check if file exist
+        _ = fs.cwd().openFile(source_file_path, .{}) catch |err| {
+            if (err == fs.File.OpenError.FileNotFound) {
+                // this can either be break for continue
+                // if you are sure that you are solving the problem in a series
+                // then break is better
+                break;
+                // else you should use continue
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    const run_cmd = b.addRunArtifact(exe);
+                // continue;
+            }
+        };
 
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    run_cmd.step.dependOn(b.getInstallStep());
+        // create an excutable for each day
+        const exe = b.addExecutable(.{
+            .name = exe_name,
+            .root_source_file = .{ .path = source_file_path },
+            .target = target,
+            .optimize = optimize,
+        });
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
+        // create install artifact for each day
+        b.installArtifact(exe);
+
+        // create cmd
+        const cmd = b.addRunArtifact(exe);
+        cmd.step.dependOn(b.getInstallStep());
+
+        // create a step
+        const des = b.fmt("Run the solution of {s}", .{day_string});
+        const step = b.step(day_string, des);
+        step.dependOn((&cmd.step));
     }
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_unit_tests = b.addRunArtifact(unit_tests);
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_unit_tests.step);
 }
